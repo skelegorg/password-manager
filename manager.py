454ee672e7633f2f8fdede842cd4ada1370db1f0
skelegorg <62708878+skelegorg@users.noreply.github.com>
2020-05-07 12:19:15 -0700

    Error fix

--- manager.py
+++ manager.py
@@ -2,24 +2,25 @@
 
 
 def accessPasswords():
-
+    print("hello")
     # create a function called first in the manager
 
 
-def initialize():
+def getPassword():
     masterPasswordFile = open("masterPasswordFile", "r")
     masterPass = masterPasswordFile.read()
     if (masterPass != ''):
         # if the master password is not empty, get user to input the master password
         masterPassInput = input("Enter your master password:")
         # if the entered password matches the password in the master
-        if(masterPassInput == masterPass):
+        if (masterPassInput == masterPass):
             # begin the normal program
+            accessPasswords()
         else:
             # tell user that their password is incorrect
             print("Password incorrect")
-            # go back to the start of the initialize function
-            initialize()
+            # go back to the start of the getPassword function
+            getPassword()
     else:
         # password does not exist, so create a password
         masterPasswordFile = open("masterPasswordFile", "w")
@@ -30,4 +31,4 @@ def initialize():
             masterPasswordFile.write(newPass1)
         else:
             print("The passwords do not match")
-        initialize()
+        getPassword()
