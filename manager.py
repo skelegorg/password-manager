18a6365fe1efbbb98acfca25616d806665c28e16
skelegorg <62708878+skelegorg@users.noreply.github.com>
2020-05-07 13:24:37 -0700

    Added password protection and password generation

--- manager.py
+++ manager.py
@@ -1,14 +1,23 @@
 # password manager
 
+# password access - only called if the user passes the getPassword function.
+
 
 def accessPasswords():
-    print("hello")
-    # create a function called first in the manager
+    # for testing purposes
+    print("hello, world!")
+
+# password protection
 
 
 def getPassword():
-    masterPasswordFile = open("masterPasswordFile", "r")
+    # open file containing master password
+    masterPasswordFile = open(
+        "C:/Users/cummi_bdpdp6n/password-manager/masterPassword.txt", "r")
+    # saves the password temporarily
     masterPass = masterPasswordFile.read()
+    masterPasswordFile.close()
+    # check if the password file is empty
     if (masterPass != ''):
         # if the master password is not empty, get user to input the master password
         masterPassInput = input("Enter your master password:")
@@ -23,12 +32,23 @@ def getPassword():
             getPassword()
     else:
         # password does not exist, so create a password
+        # open the password file up in write mode
         masterPasswordFile = open("masterPasswordFile", "w")
-        newPass1 = input("What would you like your password to be?")
+        # get the user to input their password 2x for security
+        newPass1 = input("What would you like your password to be?\n")
         newPass2 = input("Confirm your password:")
         # check if the password is the same both times
         if(newPass1 == newPass2):
+            # save the new password in the file
             masterPasswordFile.write(newPass1)
+            masterPasswordFile.close()
+            # begin the program
+            accessPasswords()
         else:
+            # if the passwords do not match, inform the user and have the user start over.
             print("The passwords do not match")
-        getPassword()
+            getPassword()
+
+
+# start the program with the getPassword function
+getPassword()
